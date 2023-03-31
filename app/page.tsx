72f10f8742aa31127175f1362148adb0aa029d86
Boban Senic <bobansenic@gmail.com>
2023-03-31 04:05:02 +0200

    some framer motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { InputPanel } from "@/components/InputPanel";
+import { motion } from "framer-motion";
 export const revalidate = 0;
 export default function Home() {
   return (
-    <main className="xl:max-w-5xl xl:mx-auto">
+    <motion.main
+      className="xl:max-w-5xl xl:mx-auto"
+      animate={{ opacity: 1, scale: 1 }}
+      initial={{ opacity: 0, scale: 0.8 }}
+      transition={{ ease: "easeOut" }}
+    >
       <InputPanel />
-    </main>
+    </motion.main>
   );
 }
